0fe68489685f9dd53a7a88aea4dbdb3a2525475a
MIguel Torres <mtorres913@gmail.com>
2023-02-22 19:00:36 -0600

    finishing submitForm function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,38 @@
 console.log('script sourced.');
+
+function getQuotes(){
+    //Axios GET request
+    axios.get('/quotes').then((response) => {
+        //Code that will run on a successful response
+        //from the server.
+       console.log(response); 
+       //quotesFromServer will be an array of quotes
+       let quotesFromServer = response.data;
+       let contentDiv = document.querySelector('#content');
+       //Loop over array of quotes and append to the DOM
+       for (let quote of quotesFromServer) {
+        contentDiv.innerHTML += `
+            <p>"${quote.text} - ${quote.author}"</p>
+        `
+       }
+    }); //ALWAYS add .catch
+}
+getQuotes();
+
+function submitForm(event){
+    //stop page from refreshing
+    event.preventDefault();
+    console.log("In submitForm function")
+    let quote = document.querySelector('#quoteInput').value
+    let author = document.querySelector('#authorInput').value
+    console.log('Inputs ', quote, author)
+    let quoteForServer = {
+        quote: quote,
+        author: author,
+    };
+    // type     url         data to send        
+    axios.post('/quotes',quoteForServer).then((response) => {
+        
+    })
+
+}
--- server/server.js
+++ server/server.js
@@ -4,8 +4,23 @@ const express = require('express');
 const app = express();
 // Heroku assigns us a unique PORT
 // Use 5001 for localhost development
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5003;
 
+let quoteList = [
+    { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+    { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+    { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+// GET request returns information
+//localhost:5003/quotes
+app.get('/quotes', (req, res)=> {
+    console.log('Get Request made for /quotes')
+     //Send back the list of quotes!
+    res.send(quoteList);
+});
+//POST request save user input
+//PUT request update information
+//DELET request to remove information
 // Look here for files
 app.use(express.static('server/public'));
 
